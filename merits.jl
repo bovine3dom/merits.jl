dc047c3670f89975272104952fd2d72b2d2dfc1e
Oliver Blanthorn <freedom4cows@gmail.com>
2024-12-11 16:24:00 +0100

    Add boxplots, active trains, explore international vs national

--- merits.jl
+++ merits.jl
@@ -1,6 +1,6 @@
 #!/bin/julia
 # prd / pop / por / arrival / departure # ignore everything else
-using DataFrames, Dates, CSV, Plots
+using DataFrames, Dates, CSV, Plots, StatsPlots
 
 raw = read("data/SKDUPD_2108_TestData.txt", String)
 
@@ -44,24 +44,28 @@ end
 # lib funcs end
 
 trains = "PRD" .* split(raw, "PRD", keepempty=false)
-tuples = mapreduce(train -> begin
-    lines = split(train, "\r\n", keepempty=false) # dos style :(
-    service = lines[1]
-    stations_raw = filter(startswith("POR+"), lines)
-    validity_raw = get(split(lines[findfirst(startswith("POP+"), lines)], ":", keepempty=true), 2, missing)
-    validities = passmissing(split)(validity_raw, "/")
-    valid_start = passmissing(Date)(passmissing(getm)(validities, 1))
-    valid_end = passmissing(Date)(passmissing(getm)(validities, 2))
-    stations = map(station -> begin
-            details = split(station, "+", keepempty=true)
-            station_code=details[2]
-            ad = split(details[3], "*", keepempty=true)
-            arrival = get(ad, 1, missing)
-            departure = get(ad, 2, missing)
-            (service=service, valid_start=valid_start, valid_end=valid_end, station_code=parse(Int,station_code), arrival=arrival, departure=departure)
-    end, stations_raw)
-    stations
-end, vcat, trains)
+tuples = (() -> begin
+    n = 0
+    mapreduce(train -> begin
+        n+=1 # unique id for each train
+        lines = split(train, "\r\n", keepempty=false) # dos style :(
+        service = lines[1]
+        stations_raw = filter(startswith("POR+"), lines)
+        validity_raw = get(split(lines[findfirst(startswith("POP+"), lines)], ":", keepempty=true), 2, missing)
+        validities = passmissing(split)(validity_raw, "/")
+        valid_start = passmissing(Date)(passmissing(getm)(validities, 1))
+        valid_end = passmissing(Date)(passmissing(getm)(validities, 2))
+        stations = map(station -> begin
+                details = split(station, "+", keepempty=true)
+                station_code=details[2]
+                ad = split(details[3], "*", keepempty=true)
+                arrival = get(ad, 1, missing)
+                departure = get(ad, 2, missing)
+                (service=service, myid=n, valid_start=valid_start, valid_end=valid_end, station_code=parse(Int,station_code), arrival=arrival, departure=departure)
+        end, stations_raw)
+        stations
+    end, vcat, trains)
+end)()
 
 
 df = DataFrame(tuples)
@@ -99,8 +103,48 @@ trains_per_hour = combine(groupby(toy, :country), g ->
 transform!(groupby(trains_per_hour, :country), :trains => (t -> t./maximum(t)) => :trains)
 trains_to_plot = trains_per_hour[nm(in.(trains_per_hour.country, Ref(top_countries.country))), :]
 
-p = plot(; margin=5*Plots.mm, xticks=Time(0):Hour(2):Time(23,59), xrotation=45, legend=:bottomright, ylabel="Trains per hour (peak = 1.0)", xlabel="Local time");
+p = plot(; margin=5*Plots.mm, xticks=Time(0):Hour(2):Time(23,59), xrotation=45, legend=:bottomright, ylabel="Station calls per hour (peak = 1.0)", xlabel="Local time");
 for country in unique(trains_to_plot.country)
     plot!(trains_to_plot[trains_to_plot.country .== country, :time], trains_to_plot[trains_to_plot.country .== country, :trains], label=country, marker=:auto, markersize=2, legend=:topleft)
 end
 p
+
+# active trains rather than station calls
+whole_trains = combine(groupby(toy, :myid), g -> begin
+    t = try
+        g[.!ismissing.(g.departure24), :][1, :]
+    catch(e)
+        g[1, :]
+    end
+    u = try
+        g[.!ismissing.(g.arrival24), :][end, :]
+    catch(e)
+        g[end, :]
+    end
+    (service=t.service, myid=t.myid, valid_start=t.valid_start, valid_end=t.valid_end, departure24=t.departure24, arrival24=u.arrival24, country_start=t.country, country_end=u.country)
+end)
+dropmissing!(whole_trains)
+# national_only = whole_trains[whole_trains.country_start .== whole_trains.country_end, :] # too few :(
+
+trains_to_plot = whole_trains[in.(whole_trains.country_start, Ref(sort!(combine(groupby(whole_trains, :country_start), nrow), :nrow, rev=true)[1:12, :country_start])), :]
+trains_to_plot = transform!(combine(groupby(trains_to_plot, :country_start), [:departure24, :arrival24] => ((d, a) -> [(time=t, trains=sum(inbounds.(t, zip(d, a)))) for t in Time(0):Minute(1):Time(23,59)]) => :active_trains), :active_trains => (t -> getfield.(t, :trains)) => :active_trains, :active_trains => (t -> getfield.(t, :time)) => :time)
+transform!(groupby(trains_to_plot, :country_start), :active_trains => (t -> t./maximum(t)) => :active_trains)
+rename!(trains_to_plot, :country_start => :country) # lazy
+rename!(trains_to_plot, :active_trains => :trains)
+p = plot(; margin=5*Plots.mm, xticks=Time(0):Hour(2):Time(23,59), xrotation=45, legend=:bottomright, ylabel="Active per hour (peak = 1.0)", xlabel="Local time");
+for country in unique(trains_to_plot.country)
+    plot!(trains_to_plot[trains_to_plot.country .== country, :time], trains_to_plot[trains_to_plot.country .== country, :trains], label=country, marker=:auto, markersize=2, legend=:topleft)
+end
+p
+
+
+# Utilisation distribution: 1.0 = peak service 24/7, 0.5 = day mostly runs at half peak service, 0 = no trains run
+p = boxplot(; margin=5*Plots.mm, legend=:none);
+# p = boxplot(; margin=5*Plots.mm, series_annotations=text.(trains_to_plot.country, :bottom, 5));
+for country in unique(trains_to_plot.country)
+    boxplot!(trains_to_plot[trains_to_plot.country .== country, :country], trains_to_plot[trains_to_plot.country .== country, :trains], label=country)
+end
+p
+
+
+# so, conclusion: can't really get much out of this sample data, mostly includes international trains, only a few countries have good data on national ones
